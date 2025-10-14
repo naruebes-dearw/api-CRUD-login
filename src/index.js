862a655c24283e2982dde51417a88dc3b4e99c47
Dearw <Dearw@myEmail.com>
2025-10-14 14:35:47 +0700

    chore: move index file into src folder

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT;
+const mongoUri = process.env.MONGO_URI;
+const jwtSecret = process.env.JWT_SECRET;
+
+console.log({ port });
+console.log({ mongoUri });
+console.log({ jwtSecret });
