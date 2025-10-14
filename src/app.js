4cd7435b208a2332c8a1dc194cfe13db06b9ee2d
Dearw <Dearw@myEmail.com>
2025-10-14 22:38:24 +0700

    feat: add app.js

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoutes);
+
+export default app;
