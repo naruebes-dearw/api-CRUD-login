a82ba04b9325a3f56a46dff8c36cd7da2c4a6672
Dearw <Dearw@myEmail.com>
2025-10-20 22:48:24 +0700

    fix: get and create user do not need to login

--- src/routes/registedUserRoutes.js
+++ src/routes/registedUserRoutes.js
@@ -9,8 +9,8 @@ import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", authMiddleware, getRegistedUsers);
-router.post("/", authMiddleware, register);
+router.get("/", getRegistedUsers);
+router.post("/", register);
 router.delete("/", authMiddleware, deleteRegistedUser);
 router.post("/login", login);
 
